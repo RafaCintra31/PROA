f423f3fa3d1aea6899e63bc5f3cca7bd5d0aebb4
RafaCintra <rafa.cintrati@gmail.com>
2026-04-01 16:09:33 -0300

    exercicios atualizados

---
+++ OneDrive - SENAC - SP/Área de Trabalho/Aula de JavaScript/js/mudando_elemento_grafico.js
@@ -0,0 +1,13 @@
+function botao_verde()
+{
+    document.querySelector(".fundo").style.backgroundColor="green";
+    document.querySelector(".fundo").style.color="white";
+    document.querySelector(".myImage").src="../img/cachorro.jpg";
+}
+
+function botao_azul()
+{
+    document.querySelector(".fundo").style.backgroundColor="blue";
+    document.querySelector(".fundo").style.color="white";
+    document.querySelector(".myImage").src="../img/tigre.webp";
+}
